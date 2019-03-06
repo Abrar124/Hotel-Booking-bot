b2830bbb3da7ddfd0385e7b56f9b161979fe4b79
abrar124 <abrar.khurshid.124@gmail.com>
2019-03-06 15:56:22 +0500

    Done text, suggestion replies and Card response

--- Desktop/Hotel Booking/functions/src/index.ts
+++ Desktop/Hotel Booking/functions/src/index.ts
@@ -5,6 +5,9 @@ const { WebhookClient } = require("dialogflow-fulfillment");
 const { Card, Suggestion } = require("dialogflow-fulfillment");
 
 // process.env.DEBUG = "dialogflow:debug";
+const imageUrl =
+  "http://www.hotel-r.net/im/hotel/asia/my/vip-hotel-segamat-1.jpg";
+const linkUrl = "https://www.hotelvipclub.com/";
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
   (request, response) => {
@@ -23,14 +26,23 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       agent.add(`I'm sorry, can you try again?`);
     }
 
-    function room(agent) {
-      agent.add("messages", [
-        {
-          platform: "facebook",
-          speech: "Text responsesssss",
-          type: 0
-        }
-      ]);
+    function roomPackage(agent) {
+      agent.add(`Choose your room type`);
+      agent.add(new Suggestion(`Basic room`));
+      agent.add(new Suggestion(`Standard room`));
+      agent.add(new Suggestion(`VIP room`));
+    }
+
+    function Hotel(agent) {
+      agent.add(
+        new Card({
+          title: `ABC Hotel`,
+          imageUrl: imageUrl,
+          text: `This is our Beautifull hotel`,
+          buttonText: "Go to website",
+          buttonUrl: linkUrl
+        })
+      );
     }
 
     // Run the proper handler based on the matched Dialogflow intent
@@ -44,10 +56,14 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
       const email = agent.parameters.email;
 
       console.log(name, persons, email);
-      agent.add(`  ${name} your request for ${persons} have forwarded we will contact you on ${email} `);
+      agent.add(
+        ` Thanks! ${name} your request for ${persons} persons have forwarded we will contact you on ${email} `
+      );
+      
     });
-    
-    intentMap.set("room", room);
+
+    intentMap.set("Room Package", roomPackage);
+    intentMap.set("Hotel", Hotel);
 
     _agent.handleRequest(intentMap);
   }
